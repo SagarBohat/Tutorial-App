de3e14f7b24133cb4bb01ca9cff3a3208913a193
SagarBohat <43495053+SagarBohat@users.noreply.github.com>
2018-09-22 05:05:53 -0700

    Create AboutAndroid.java

---
+++ AboutAndroid.java
@@ -0,0 +1,13 @@
+package com.example.asd.androidbasics;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AboutAndroid extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about_android);
+    }
+}
